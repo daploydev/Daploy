ee04d508d68e95f06b1847405d806db6db18cc19
daploydev <59704289+daploydev@users.noreply.github.com>
2020-09-29 20:48:24 -0400

    Update Layout.js

--- daploy/components/Layout.js
+++ daploy/components/Layout.js
@@ -1,39 +1,38 @@
-import Head from "next/head";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import Link from "next/link";
+import Head from 'next/head';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import Link from 'next/link';
 
 const Layout = (pageProps) => {
-  return (
-    <div>
-      <Head>
-        <title>daploy.dev</title>
-      </Head>
-      <Header></Header>
-      <Header></Header>
-      <Header></Header>
-      <Header></Header>
-      <Header></Header>
-      <Header></Header>
-      <Header></Header>
-      <Header></Header>
-      <Link href="/login">
-        <a>
-          <Header />
-        </a>
-      </Link>
-      <Header></Header>
-      <Header></Header>
-      <Header></Header>
-      <Header></Header>
-      <Header></Header>
-      <Header></Header>
-      <Header></Header>
-      <Header></Header>
-      <div className="container">{pageProps.children}</div>
-      <Footer />
-    </div>
-  );
+	return (
+		<div>
+			<Head>
+				<title>daploy.dev</title>
+			</Head>
+			<Header></Header>
+			<Header></Header>
+			<Header></Header>
+			<Header></Header>
+			<Header></Header>
+			<Header></Header>
+			<Header></Header>
+			<Header></Header>
+			<Link href="/login">
+				<a>
+					<Header />
+				</a>
+			</Link>
+			<Header></Header>
+			<Header></Header>
+			<Header></Header>
+			<Header></Header>
+			<Header></Header>
+			<Header></Header>
+			<Header></Header>
+			<Header></Header>
+			<div className="container">{pageProps.children}</div>
+		</div>
+	);
 };
 
 export default Layout;
