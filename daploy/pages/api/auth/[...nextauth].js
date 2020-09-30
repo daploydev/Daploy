2869d10a9164b9ef022a7e56da3267e62016275d
daploydev <59704289+daploydev@users.noreply.github.com>
2020-09-29 20:39:53 -0400

    Update [...nextauth].js

--- daploy/pages/api/auth/[...nextauth].js
+++ daploy/pages/api/auth/[...nextauth].js
@@ -1,18 +1,18 @@
-import NextAuth from "next-auth";
-import Providers from "next-auth/providers";
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
 
 const options = {
-  // Configure one or more authentication providers
-  providers: [
-    Providers.GitHub({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-    // ...add more providers here
-  ],
+	// Configure one or more authentication providers
+	providers: [
+		Providers.Email({
+			server: process.env.EMAIL_SERVER,
+			from: process.env.EMAIL_FROM,
+		}),
+		// ...add more providers here
+	],
 
-  // A database is optional, but required to persist accounts in a database
-  database: process.env.DATABASE_URL,
+	// A database is optional, but required to persist accounts in a database
+	database: process.env.DATABASE_URL,
 };
 
 export default (req, res) => NextAuth(req, res, options);
