2e0edc6fd65e82be2ac40b5572124c1bacf986e3
daploydev <59704289+daploydev@users.noreply.github.com>
2020-10-02 18:17:54 -0400

    Update _app.js

--- daploy/pages/_app.js
+++ daploy/pages/_app.js
@@ -1,51 +1,51 @@
-import * as Realm from 'realm-web';
-import Layout from '../components/Layout';
-import 'fontsource-roboto';
+import * as Realm from "realm-web";
+import Layout from "../components/Layout";
+import "fontsource-roboto";
 
-const REALM_APP_ID = 'daploy-wcred';
+const REALM_APP_ID = "daploy-wcred";
 const app = new Realm.App({ id: REALM_APP_ID });
 
-const mongo = app.services.mongodb('mongodb-atlas');
-const mongoCollection = mongo.db('daploy').collection('items');
+const mongo = app.services.mongodb("mongodb-atlas");
+const mongoCollection = mongo.db("daploy").collection("items");
 
 function GetItems({ setItem }) {
-	const Items = async () => {
-		const queryFilter = {};
-		const itemDoc = await mongoCollection.findOne(queryFilter);
-		setItem(itemDoc);
-	};
-	return <div onClick={Items()}></div>;
+  const Items = async () => {
+    const queryFilter = {};
+    const itemDoc = await mongoCollection.findOne(queryFilter);
+    setItem(itemDoc);
+  };
+  return <div onClick={Items()}></div>;
 }
 // Create a component that lets an anonymous user log in
 function Login({ setUser }) {
-	const loginAnonymous = async () => {
-		const user = await app.logIn(Realm.Credentials.anonymous());
-		setUser(user);
-	};
-	return <div onClick={loginAnonymous()}></div>;
+  const loginAnonymous = async () => {
+    const user = await app.logIn(Realm.Credentials.anonymous());
+    setUser(user);
+  };
+  return <div onClick={loginAnonymous()}></div>;
 }
 const App = ({ Component, pageProps }) => {
-	// Keep the logged in Realm user in local state. This lets the app re-render
-	// whenever the current user changes (e.g. logs in or logs out).
-	const [user, setUser] = React.useState(app.currentUser);
-	const [item, setItem] = React.useState('');
+  // Keep the logged in Realm user in local state. This lets the app re-render
+  // whenever the current user changes (e.g. logs in or logs out).
+  const [user, setUser] = React.useState(app.currentUser);
+  const [item, setItem] = React.useState("");
 
-	// If a user is logged in, show their details.
-	// Otherwise, show the login screen.
-	return (
-		<Layout {...pageProps}>
-			<div className="App-header">
-				{user ? (
-					<div>
-						<GetItems setItem={setItem} />
-						{JSON.stringify(app.currentUser)}
-					</div>
-				) : (
-					<Login setUser={setUser} />
-				)}
-			</div>
-			<Component {...pageProps} />
-			<style jsx global>{`
+  // If a user is logged in, show their details.
+  // Otherwise, show the login screen.
+  return (
+    <Layout {...pageProps}>
+      <div className="App-header">
+        {user ? (
+          <div>
+            <GetItems setItem={setItem} />
+            {JSON.stringify(app.currentUser)}
+          </div>
+        ) : (
+          <Login setUser={setUser} />
+        )}
+      </div>
+      <Component {...pageProps} />
+      <style jsx global>{`
         body {
           margin: 0px;
           padding: 0px;
@@ -54,8 +54,8 @@ const App = ({ Component, pageProps }) => {
           background-color: #08AEEA;
           background-image: linear-gradient(217deg, #08AEEA 0%, #2AF598 100%);
       `}</style>
-		</Layout>
-	);
+    </Layout>
+  );
 };
 
 export default App;
