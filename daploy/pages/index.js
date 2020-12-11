c59e61e2658678923392cdc660efb7555521169b
daploydev <59704289+daploydev@users.noreply.github.com>
2020-12-11 18:10:30 -0500

    tap gesture

--- daploy/pages/index.js
+++ daploy/pages/index.js
@@ -37,6 +37,17 @@ export default function Home() {
 								],
 								transition: { duration: 0.2 },
 							}}
+							whileTap={{
+								scale: [1, 1.4, 1.2],
+								rotate: [0, 10, -10, 0],
+								filter: [
+									"hue-rotate(0) contrast(100%",
+									"hue-rotate(360deg) contrast(200%)",
+									"hue-rotate(45deg) contrast(300%)",
+									"hue-rotate(0) contrast(100%)",
+								],
+								transition: { duration: 0.2 },
+							}}
 							src="/images/daploy-logo.png"
 							width="50%"
 							class="text"
