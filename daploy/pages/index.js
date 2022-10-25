9957f35130885e2ca746c0d1a22b5eb0fce53ba6
daploydev <59704289+daploydev@users.noreply.github.com>
2022-10-25 16:12:32 -0400

    nextjs 13 upgrade

--- daploy/pages/index.js
+++ daploy/pages/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { motion } from "framer-motion";
-import { Grid, Typography, Button } from "@material-ui/core";
-import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
@@ -24,48 +22,38 @@ export default function Home() {
 					<div class="blob"></div>
 				</div>
 				<Link href="/home" as="/home">
-					<a>
-						<motion.img
-							whileHover={{
-								scale: [1, 1.4, 1.2],
-								rotate: [0, 10, -10, 0],
-								filter: [
-									"hue-rotate(0) contrast(100%",
-									"hue-rotate(360deg) contrast(200%)",
-									"hue-rotate(45deg) contrast(300%)",
-									"hue-rotate(0) contrast(100%)",
-								],
-								transition: { duration: 0.2 },
-							}}
-							whileTap={{
-								scale: [1, 1.4, 1.2],
-								rotate: [0, 10, -10, 0],
-								filter: [
-									"hue-rotate(0) contrast(100%",
-									"hue-rotate(360deg) contrast(200%)",
-									"hue-rotate(45deg) contrast(300%)",
-									"hue-rotate(0) contrast(100%)",
-								],
-								transition: { duration: 0.2 },
-							}}
-							src="/images/daploy-logo.png"
-							width="50%"
-							className="text"
-						/>
-					</a>
+					<motion.img
+						whileHover={{
+							scale: [1, 1.4, 1.2],
+							rotate: [0, 10, -10, 0],
+							filter: [
+								"hue-rotate(0) contrast(100%",
+								"hue-rotate(360deg) contrast(200%)",
+								"hue-rotate(45deg) contrast(300%)",
+								"hue-rotate(0) contrast(100%)",
+							],
+							transition: { duration: 0.2 },
+						}}
+						whileTap={{
+							scale: [1, 1.4, 1.2],
+							rotate: [0, 10, -10, 0],
+							filter: [
+								"hue-rotate(0) contrast(100%",
+								"hue-rotate(360deg) contrast(200%)",
+								"hue-rotate(45deg) contrast(300%)",
+								"hue-rotate(0) contrast(100%)",
+							],
+							transition: { duration: 0.2 },
+						}}
+						src="/images/daploy-logo.png"
+						width="50%"
+						className="text"
+					/>
 				</Link>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					version="1.1"
-					height="0"
-				>
+				<svg xmlns="http://www.w3.org/2000/svg" version="1.1" height="0">
 					<defs>
 						<filter id="goog">
-							<feGaussianBlur
-								in="SourceGraphic"
-								stdDeviation="10"
-								result="blur"
-							/>
+							<feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
 							<feColorMatrix
 								in="blur"
 								mode="matrix"
@@ -75,11 +63,7 @@ export default function Home() {
 							<feBlend in="SourceGraphic" in2="goo" />
 						</filter>
 						<filter id="goo">
-							<feGaussianBlur
-								in="SourceGraphic"
-								stdDeviation="10"
-								result="blur"
-							/>
+							<feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
 							<feColorMatrix
 								in="blur"
 								mode="matrix"
